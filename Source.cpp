2a4840bcbfd39cb66da18d874bb1b8f302916126
Daniil Osokin <90867530+eeeeagle@users.noreply.github.com>
2022-06-06 23:01:55 +0400

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,9 +1,30 @@
-#include "Tree.hpp"
-#include <map>
+#include "AVL_Tree.hpp"
 
 int main()
 {
-	Map<int, char> map;
-	map.insert(4, 'd');
+	AVL_Tree map;
+
+	map.insert(4, "ss");
+	map.insert(2, "ss");
+	map.insert(7, "ss");
+	map.insert(4, "ss");
+	map.insert(3, "ss");
+	map.insert(6, "ss");
+	map.insert(-7, "ss");
+	map.insert(1, "ss");
+	map.insert(-4, "ss");
+	map.insert(9, "ss");
+	map.insert(15, "ss");
+	map.insert(12, "ss");
+	map.insert(5, "ss");
+
+	map.print_debug();
+
+	std::cout << map.find(9) << std::endl;
+	map.erase(9);
+	std::cout << map.find(9) << std::endl;
+
+	map.print_debug();
+
 	return 0;
 }

80970fec6420fed2ac12f8f08c3463f395cec0c0
Daniil Osokin <90867530+eeeeagle@users.noreply.github.com>
2022-09-08 22:33:04 +0400

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -3,112 +3,107 @@
 
 int get_key()
 {
-	int key = _getch();
-	if ((key == 0) || (key == 224))
-		key = _getch();
-	return key;
+    int key = _getch();
+    if ((key == 0) || (key == 224))
+        key = _getch();
+    return key - 48;
 }
 
 int tree_menu_info()
 {
-	std::cout << "  [1] Insert" << std::endl;
-	std::cout << "  [2] Delete" << std::endl;
-	std::cout << "  [3] Find"   << std::endl;
-	std::cout << "  [4] Print"  << std::endl;
-	std::cout << "[ESC] Return" << std::endl;
-	
-	while (true)
-	{
-		int key = get_key();
-		if (key == 49 || key == 50 || key == 51 || key == 52 || key == 27)
-			return key;
-	}
+    std::cout << "[1] Insert" << std::endl;
+    std::cout << "[2] Delete" << std::endl;
+    std::cout << "[3] Find"   << std::endl;
+    std::cout << "[4] Print"  << std::endl;
+    std::cout << "[5] Return" << std::endl;
+    
+    while (true)
+    {
+        int key = get_key();
+        if (key == 1 || key == 2 || key == 3 || key == 4 || key == 5)
+            return key;
+    }
 }
 
 void tree_menu(AVL_Tree& tree)
 {
-	while (true)
-	{
-		int key = tree_menu_info();
-		system("cls");
-		if (key == 27)
-		{
-			return;
-		}
-		else if (key == 49)
-		{
-			std::cout << "   Enter key: ";
-			std::cin >> key;
-				
-			std::string value;
-			std::cout << "Enter string: ";
-			std::cin >> value;
+    while (true)
+    {
+        int key = tree_menu_info();
+        system("cls");
+        if (key == 5)
+        {
+            return;
+        }
+        else if (key == 1)
+        {
+            std::cout << "   Enter key: ";
+            std::cin >> key;
+                
+            std::string value;
+            std::cout << "Enter string: ";
+            std::cin >> value;
 
-			tree.insert(key, value);
-		}
-		else if (key == 50)
-		{
-			std::cout << "Enter key: ";
-			std::cin >> key;
-			tree.erase(key);
-		}
-		else if (key == 51)
-		{
-			std::cout << "Enter key: ";
-			std::cin >> key;
+            tree.insert(key, value);
+        }
+        else if (key == 2)
+        {
+            std::cout << "Enter key: ";
+            std::cin >> key;
+            tree.erase(key);
+        }
+        else if (key == 3)
+        {
+            std::cout << "Enter key: ";
+            std::cin >> key;
 
-			std::cout << tree.find(key) << std::endl;
-			system("pause");
-		}
-		else if (key == 52)
-		{
-			tree.print();
-			system("pause");
-		}
-		system("cls");
-	}
+            std::cout << tree.find(key) << std::endl;
+            system("pause");
+        }
+        else if (key == 4)
+        {
+            tree.print();
+            system("pause");
+        }
+        system("cls");
+    }
 }
 
 int main_info()
 {
-	std::cout << "  [1] Tree 1 menu"		   << std::endl;
-	std::cout << "  [2] Tree 2 menu"		   << std::endl;
-	std::cout << "  [3] Copy tree 1 to tree 2" << std::endl;
-	std::cout << "[ESC] Exit"				   << std::endl;
+    std::cout << "[1] Tree menu"        << std::endl;
+    std::cout << "[2] Copy Tree menu"   << std::endl;
+    std::cout << "[3] Exit"             << std::endl;
 
-	while (true)
-	{
-		int key = get_key();
-		if (key == 49 || key == 50 || key == 51 || key == 27)
-			return key;
-	}
+    while (true)
+    {
+        int key = get_key();
+        if (key == 1 || key == 2 || key == 3)
+            return key;
+    }
 }
 
 int main()
 {
-	AVL_Tree tree_1;
-	AVL_Tree tree_2;
+    AVL_Tree tree;
 
-	while (true)
-	{
-		int key = main_info();
-		system("cls");
-		if (key == 27)
-		{
-			return 0;
-		}
-		else if (key == 49)
-		{
-			tree_menu(tree_1);
-		}
-		else if (key == 50)
-		{
-			tree_menu(tree_2);
-		}
-		else if (key == 51)
-		{
-			tree_2 = AVL_Tree(tree_1);
-		}
-		system("cls");
-	}
+    while (true)
+    {
+        int key = main_info();
+        system("cls");
+        if (key == 3)
+        {
+            return 0;
+        }
+        else if (key == 1)
+        {
+            tree_menu(tree);
+        }
+        else if (key == 2)
+        {
+            AVL_Tree tree_copy(tree);
+            tree_menu(tree_copy);
+        }
+        system("cls");
+    }
 }
